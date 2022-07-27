c31dd8f2ce6786ec0340397a904cd8d79ee6a166
kozyrek <kozyrevaia@bk.ru>
2022-07-28 00:56:29 +0500

    правки по критериям

--- source/js/modules/open-menu.js
+++ source/js/modules/open-menu.js
@@ -1,18 +1,47 @@
+const page = document.querySelector('.page-body');
 const menuElem = document.querySelector('.page-header');
 const titleElem = menuElem.querySelector('.page-header__toggle');
 const linkElements = menuElem.querySelectorAll('.no-js__link');
+const menu = document.querySelector('.page-header__container');
+const focusableElementsString = 'a[href], button:not([disabled])';
+const focusableElements = menu.querySelectorAll(focusableElementsString);
+
+const menuElements = Array.from(focusableElements);
+const firstTabStop = menuElements[0];
+const lastTabStop = menuElements[menuElements.length - 1];
+
+const setClass = () => {
+  menuElem.classList.toggle('active');
+  page.classList.toggle('scroll-hidden');
+};
 
 const openMenu = () => {
   titleElem.addEventListener('click', function (item) {
     item.preventDefault();
-    menuElem.classList.toggle('active');
+    setClass();
   });
 
   for (let i = 0; i < linkElements.length; i++) {
     linkElements[i].addEventListener('click', function () {
-      menuElem.classList.toggle('active');
+      setClass();
     });
   }
+
+  menu.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 9) {
+      if (evt.shiftKey) {
+        if (document.activeElement === firstTabStop) {
+          evt.preventDefault();
+          lastTabStop.focus();
+        }
+      } else {
+        if (document.activeElement === lastTabStop) {
+          evt.preventDefault();
+          firstTabStop.focus();
+        }
+      }
+    }
+  });
 };
 
 export {openMenu};
